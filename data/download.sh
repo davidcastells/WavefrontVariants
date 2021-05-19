e82e4d43adce00b8028c24ef3ee5fb2f37fcc1b3
U-TPY14\dcr <david.castells@uab.cat>
2021-05-19 10:20:30 +0200

    new files

--- data/download.sh
+++ data/download.sh
@@ -1,5 +1,7 @@
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AF133821.1&rettype=fasta" -O "AF133821.1.fasta"
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AY352275.1&rettype=fasta" -O "AY352275.1.fasta"
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000898.1&rettype=fasta" -O "NC_000898.1.fasta"
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_007605.1&rettype=fasta" -O NC_007605.1.fasta"
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AF133821.1&rettype=fasta" -O AF133821.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AY352275.1&rettype=fasta" -O AY352275.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000898.1&rettype=fasta" -O NC_000898.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_007605.1&rettype=fasta" -O NC_007605.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AE016879.1&rettype=fasta" -O AE016879.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AE017225.1&rettype=fasta" -O AE017225.1.fasta
 
