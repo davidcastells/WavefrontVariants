d4403537e445c6c4773ab928d3d49dae6b2745c9
David Castells-Rufas <david.castells@uab.cat>
2021-12-20 20:32:58 +0100

    100 invocations before read

--- OCLGPUWavefrontOriginal2Cols.cpp
+++ OCLGPUWavefrontOriginal2Cols.cpp
@@ -143,7 +143,7 @@ void OCLGPUWavefrontOriginal2Cols::progress(PerformanceLap& lap, long r, int& la
     }
 }
 
-#define NUMBER_OF_INVOCATIONS_PER_READ 10
+#define NUMBER_OF_INVOCATIONS_PER_READ 100
 
 long OCLGPUWavefrontOriginal2Cols::getDistance()
 {
@@ -168,7 +168,7 @@ long OCLGPUWavefrontOriginal2Cols::getDistance()
 
         progress(lap, r, lastpercent, cellsAllocated, cellsAlive);
         
-        if (r % NUMBER_OF_INVOCATIONS_PER_READ == 0)
+        if ((r % NUMBER_OF_INVOCATIONS_PER_READ) == 0)
         {
             m_queue->readBuffer(m_buf_final_d_r, m_final_d_r, 2 * sizeof(long));
             
