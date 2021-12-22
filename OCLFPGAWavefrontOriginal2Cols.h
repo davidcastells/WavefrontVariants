29409356ce88e8c66c89a955d3ce429fe6d2335e
David Castells-Rufas <david.castells@uab.cat>
2021-12-22 19:59:38 +0100

    FPGA versions

---
+++ OCLFPGAWavefrontOriginal2Cols.h
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   OCLFPGAWavefrontOriginal2Cols.h
+ * Author: dcr
+ *
+ * Created on December 22, 2021, 7:24 PM
+ */
+
+#ifndef OCLFPGAWAVEFRONTORIGINAL2COLS_H
+#define OCLFPGAWAVEFRONTORIGINAL2COLS_H
+
+#include "Aligner.h"
+#include "OCLUtils.h"
+
+class OCLFPGAWavefrontOriginal2Cols : public Aligner
+{
+public:
+    OCLFPGAWavefrontOriginal2Cols();
+    virtual ~OCLFPGAWavefrontOriginal2Cols();
+
+    void setInput(const char* P, const char* T, long k);
+    long getDistance();
+    char* getAlignmentPath(long* distance);
+    const char* getDescription(); 
+
+    void setCommonArgs();
+    void invokeKernel(long r);
+    void progress(PerformanceLap& lap, long r, int& lastpercent, long cellsAllocated, long cellsAlive);
+    
+	
+    long* m_W;
+    const char* m_P;
+    const char* m_T;
+    long m_n;
+    long m_m;
+    long m_k;
+    long m_top;
+    long m_final_d_r[2];
+    
+    cl_context m_context;
+    OCLQueue* m_queue;
+    cl_mem m_buf_P;
+    cl_mem m_buf_T;
+    cl_mem m_buf_W;
+    cl_mem m_buf_final_d_r;
+    cl_kernel m_kernel;
+
+};
+
+#endif /* OCLFPGAWAVEFRONTORIGINAL2COLS_H */
+
