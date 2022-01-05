cd7f1ce5c2c4c02b18c108896a2291cb741ea42c
David Castells-Rufas <david.castells@uab.cat>
2022-01-05 16:21:00 +0100

    big sequences from CUDAlign

--- data/download.sh
+++ data/download.sh
@@ -4,6 +4,8 @@ wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_007605.1&rettype=fasta" -O NC_007605.1.fasta
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AE016879.1&rettype=fasta" -O AE016879.1.fasta
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AE017225.1&rettype=fasta" -O AE017225.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_005027.1&rettype=fasta" -O NC_005027.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_003997.3&rettype=fasta" -O NC_003997.3.fasta 
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=BA000046.3&rettype=fasta" -O BA000046.3.fasta
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000021.7&rettype=fasta" -O NC_000021.7.fasta
 
