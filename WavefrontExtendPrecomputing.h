b0c6cd0ac3b053ebd89d7ea9b5417058e477a6ec
David Castells-Rufas <david.castells@uab.cat>
2022-01-05 13:14:01 +0100

    inherit member variables from Aligner

--- WavefrontExtendPrecomputing.h
+++ WavefrontExtendPrecomputing.h
@@ -8,29 +8,26 @@
 class WavefrontExtendPrecomputing : public Aligner
 {
 public:
-	WavefrontExtendPrecomputing();
-	virtual ~WavefrontExtendPrecomputing();
+    WavefrontExtendPrecomputing();
+    virtual ~WavefrontExtendPrecomputing();
 
-	void setInput(const char* P, const char* T, long k);
-	void precomputeExtend();
-	long getDistance();
-	char* getAlignmentPath(long* distance);
-	const char* getDescription(); 
+    void setInput(const char* P, const char* T, long k);
+    void precomputeExtend();
+    long getDistance();
+    char* getAlignmentPath(long* distance);
+    const char* getDescription(); 
 
-	
-	int polarExistsInD(int d, int r);
-	int polarExistsInW(int d, int r);
 
-	int* m_W;
-	long m_size_W;
-	int* m_E;
-	long m_size_E;
-	const char* m_P;
-	const char* m_T;
-	long m_n;
-	long m_m;
-	long m_k;
-	long m_top;
+    int polarExistsInD(int d, int r);
+    int polarExistsInW(int d, int r);
+
+    int* m_W;
+    long m_size_W;
+    int* m_E;
+    long m_size_E;
+
+    long m_k;
+    long m_top;
 };
 
 #endif
