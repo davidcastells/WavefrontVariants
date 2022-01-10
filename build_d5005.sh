628307d33569ed9b6a9170e3a8df90c413ff4e51
David Castells-Rufas <david.castells@uab.cat>
2022-01-10 13:48:07 +0100

    TILE_LEN is a constant for FPGAs

--- build_d5005.sh
+++ build_d5005.sh
@@ -2,4 +2,4 @@
 source /data/intel_fpga/devcloudLoginToolSetup.sh
 tools_setup -t S10DS 
 cd ~/INT_Bioinformatics/WavefrontVariants
-aoc WFO2ColsFPGA.cl > compile_d5005.log
+aoc WFO2ColsFPGA.cl -D TILE_LEN=3 -D EXTEND_ALIGNED -D WORKGROUP_SIZE=8 > compile_d5005.log
