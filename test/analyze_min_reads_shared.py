6dfe261b3f1dde38ba1564edfb89afac0df467c9
David Castells-Rufas <david.castells@uab.cat>
2022-05-03 10:36:15 +0200

    testing

---
+++ test/analyze_min_reads_shared.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy
+import os
+
+df = pd.read_csv('min_reads_shared.txt', skiprows=1)
+
+df_shared = df[df['mem'] == 'shared']
+df_global = df[df['mem'] == 'global']
+
+
+x_shared = np.array(df_shared['wgs'])
+y_shared = np.array(df_shared['time'])
+gcups_shared = np.array(df_shared['gcups'])
+
+x_global = np.array(df_global['wgs'])
+y_global = np.array(df_global['time'])
+gcups_global = np.array(df_global['gcups'])
+
+plt.plot(x_shared, y_shared, label='shared')
+plt.plot(x_global, y_global, label='global')
+
+
+#plt.xlim(0, d)
+#plt.ylim(0, 0.2)
+
+#plt.title('Kernel execution time as a function of number of Workitems')
+plt.ylabel('Execution time (s)')
+plt.xlabel('Workgroup Size')
+plt.legend()
+plt.savefig('min_reads_shared.svg')
+plt.show()
+
+
+cmd = 'inkscape min_reads_shared.svg --export-area-drawing --export-pdf=min_reads_shared.pdf'
+print(cmd)
+os.system(cmd)
+
+
+plt.plot(x_shared, gcups_shared, label='shared')
+plt.plot(x_global, gcups_global, label='global')
+
+
+#plt.xlim(0, d)
+#plt.ylim(0, 0.2)
+
+#plt.title('Kernel execution time as a function of number of Workitems')
+plt.ylabel('GCUPS')
+plt.xlabel('Workgroup Size')
+plt.legend()
+plt.savefig('min_reads_shared_gcups.svg')
+plt.show()
+
---
+++ test/test_min_reads_shared.py
@@ -0,0 +1,84 @@
+import os
+
+def split_multiple(s, seps):
+    res = [s]
+    for sep in seps:
+        s, res = res, []
+        for seq in s:
+            res += seq.split(sep)
+    return res
+    
+    
+def test(f1, f2, mem, wgs):
+    cmd = '../wavefront -pid 1 -WFO2OCL -fP ../data/{} -fT ../data/{} -wgs {} --local-tile-memory {} -v -k -1 -pp 10'.format(f1,f2, wgs, mem)
+    #print(cmd)
+    p = os.popen(cmd)
+    res = p.read()
+    lines = res.splitlines()
+    for line in lines:
+        #print(line)
+        if (('m=' in line) and ('n=' in line)):
+            part = split_multiple(line, '= ')
+            m = int(part[1])
+            n = int(part[3])
+            #print('M=', m, 'N=', n)
+            
+        if (('ptxas info' in line) and ('Used' in line)):
+            #print(line)
+            part = line.split(':')[1]
+            parts = part.split(',')
+            
+            registers = 0
+            shmem = 0
+            cmem = 0
+            
+            for part in parts:
+                if ('registers' in part):
+                    registers = int(part.strip().split(' ')[1])
+                if ('smem' in part):
+                    shmem = int(part.strip().split(' ')[0])
+                if ('cmem' in part):
+                    cmem += int(part.strip().split(' ')[0])
+
+            
+        if (('Distance=' in line) and ('Time=' in line)):
+            part = split_multiple(line, '=')
+            #print(part)
+            distance = int(part[1].split()[0])
+            time = float(part[2].split()[0])
+            #print('D=', distance, 'T=', time)
+        
+    gcups = m*n/time/1E9
+    
+    print('{},{},{},{},{},{},{}'.format(mem, wgs,registers, shmem, cmem, time, gcups))
+    
+def testLong(f1, f2, i):
+    cmd = '../wavefront -pid 1 -WFO2OCL -fP ../data/{} -fT ../data/{} -wgs {} -v -k -1 -pp 10'.format(f1,f2, i)
+    print(cmd)
+    os.system(cmd)
+    
+# CUDAlign 1.0 tests
+
+os.system('export CUDA_CACHE_DISABLE=1'); 
+
+print('Testing BA000035.2.fasta vs BX927147.1.fasta')
+print('mem,wgs,register,shmem,cmem,time,gcups')
+
+for wgs in range(32,1025, 32):
+    for mem in ['shared','global']:
+        test('BA000035.2.fasta', 'BX927147.1.fasta', mem, wgs)
+
+
+
+#test('NC_000898.1.fasta', 'NC_007605.1.fasta')
+#testLong('NC_003064.2.fasta', 'NC_000914.1.fasta')
+#test('CP000051.1.fasta', 'AE002160.2.fasta')
+#testED('AE016879.1.fasta', 'AE017225.1.fasta')
+#testED('NC_005027.1.fasta', 'NC_003997.3.fasta')
+#test('NT_033779.4.fasta', 'NT_037436.3.fasta', -1) 
+#testED('BA000046.3.fasta', 'NC_000021.7.fasta') 
+
+
+
+
+
