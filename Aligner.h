0370c9e11967dc0ebe9e1ce8be93a673b9e5d6d8
David Castells-Rufas <david.castells@uab.cat>
2022-05-10 20:40:35 +0200

    support for configurable int type

--- Aligner.h
+++ Aligner.h
@@ -23,12 +23,19 @@
 
 #include <stdio.h>
 
+#define INT_TYPE int
+#define INT_TYPE_FMT d
+#define STR(x)  #x
+#define STR_INT_TYPE STR(int)
+#define FMT_INT_TYPE STR(INT_TYPE_FMT)
+#define CL_INT_TYPE cl_int
+
 class Aligner
 {
 public:
-    virtual void setInput(const char* P, const char* T, long k) = 0;
-    virtual long getDistance() = 0;
-    virtual char* getAlignmentPath(long* distance) = 0;
+    virtual void setInput(const char* P, const char* T, INT_TYPE k) = 0;
+    virtual INT_TYPE getDistance() = 0;
+    virtual char* getAlignmentPath(INT_TYPE* distance) = 0;
     virtual const char* getDescription() = 0; 
 
     void freePath(char* p)
@@ -44,7 +51,7 @@ public:
             PerformanceLap lap;
             if (doAlignmentPath)
             {
-                    long ed;
+                    INT_TYPE ed;
                     char* path = getAlignmentPath(&ed);
                     lap.stop();
 
@@ -54,15 +61,15 @@ public:
                     printf("%s\n", path);
                     //printf(gP);
                     //printf("\n");
-                    printf("%s Distance=%ld Time=%0.5f seconds\n", getDescription(),  ed, lap.lap());
+                    printf("%s Distance=%d Time=%0.5f seconds\n", getDescription(),  ed, lap.lap());
                     freePath(path);
             }
             else
             {
-                    long ed = getDistance();
+                    INT_TYPE ed = getDistance();
                     lap.stop();
-                    printf("%s Distance=%ld Time=%0.5f seconds\n", getDescription(), ed, lap.lap());
-                    printf("m x n = %ld x %ld\n", m_m, m_n);
+                    printf("%s Distance=%d Time=%0.5f seconds\n", getDescription(), ed, lap.lap());
+                    printf("m x n = %d x %d\n", m_m, m_n);
                     printf("Equivalent GCUPS: %f\n", (((double)m_m*m_n)/(lap.lap() * 1E9)));
             }
     }
@@ -71,8 +78,8 @@ public:
 protected:        
     const char* m_P;
     const char* m_T;
-    long m_n;
-    long m_m;
+    INT_TYPE m_n;
+    INT_TYPE m_m;
 
 };
 
