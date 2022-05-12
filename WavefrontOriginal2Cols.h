5c4e1529992487b608128bfd82c28048ea03da5d
David Castells-Rufas <david.castells@uab.cat>
2022-05-12 17:02:41 +0200

    int type

--- WavefrontOriginal2Cols.h
+++ WavefrontOriginal2Cols.h
@@ -11,21 +11,17 @@ public:
     WavefrontOriginal2Cols();
     virtual ~WavefrontOriginal2Cols();
 
-    void setInput(const char* P, const char* T, long k);
-    long getDistance();
-    char* getAlignmentPath(long* distance);
+    void setInput(const char* P, const char* T, INT_TYPE k);
+    INT_TYPE getDistance();
+    char* getAlignmentPath(INT_TYPE* distance);
     const char* getDescription(); 
 
 protected:
-    void progress(PerformanceLap& lap, long r, int& lastpercent, long cellsAllocated, long cellsAlive);
+    void progress(PerformanceLap& lap, INT_TYPE r, int& lastpercent, long cellsAllocated, long cellsAlive);
 
-    long* m_W;
-    const char* m_P;
-    const char* m_T;
-    long m_n;
-    long m_m;
-    long m_k;
-    long m_top;
+    INT_TYPE* m_W;
+    INT_TYPE m_k;
+    INT_TYPE m_top;
 };
 
 #endif
