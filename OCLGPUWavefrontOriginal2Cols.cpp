8cf0a6a522c217965c1ac52cac8ee46086ab12ea
David Castells-Rufas <david.castells@uab.cat>
2022-05-12 17:03:08 +0200

    int type

--- OCLGPUWavefrontOriginal2Cols.cpp
+++ OCLGPUWavefrontOriginal2Cols.cpp
@@ -43,8 +43,14 @@
 
 extern int verbose;
 extern int gPid;
-extern int gMeasureIterationTime;
+extern int gDid;
+extern int gTileLen;
 extern int gWorkgroupSize;
+extern int gExtendAligned;
+extern int gEnqueuedInvocations;
+extern double gPrintPeriod;
+extern char* gLocalTileMemory;
+extern int gMeasureIterationTime;
 
 OCLGPUWavefrontOriginal2Cols::OCLGPUWavefrontOriginal2Cols()
 {
@@ -75,7 +81,7 @@ OCLGPUWavefrontOriginal2Cols::~OCLGPUWavefrontOriginal2Cols()
         delete [] m_W;
 }
 
-void OCLGPUWavefrontOriginal2Cols::setInput(const char* P, const char* T, long k)
+void OCLGPUWavefrontOriginal2Cols::setInput(const char* P, const char* T, INT_TYPE k)
 {
     // this should not be allocated, we only expect a single call
     assert(m_W == NULL);
@@ -89,7 +95,7 @@ void OCLGPUWavefrontOriginal2Cols::setInput(const char* P, const char* T, long k
 
     try
     {
-        m_W = new long[size];
+        m_W = new INT_TYPE[size];
     }
     catch (const std::bad_alloc& e) 
     {
@@ -111,42 +117,65 @@ void OCLGPUWavefrontOriginal2Cols::setInput(const char* P, const char* T, long k
 
     printf("creating buffer %.2f GB\n", size*sizeof(long)/(1E9));
 
-    m_buf_W = clCreateBuffer(m_context, CL_MEM_READ_WRITE, size * sizeof(long), NULL, &err);
+    m_buf_W = clCreateBuffer(m_context, CL_MEM_READ_WRITE, size * sizeof(INT_TYPE), NULL, &err);
     CHECK_CL_ERRORS(err);
     
-    m_buf_final_d_r = clCreateBuffer(m_context, CL_MEM_READ_WRITE, sizeof(long), NULL, &err);
+    m_buf_final_d_r = clCreateBuffer(m_context, CL_MEM_READ_WRITE, sizeof(INT_TYPE), NULL, &err);
     CHECK_CL_ERRORS(err);
     
     auto ocl = OCLUtils::getInstance();
-    std::string options = "";
+    
+    std::string options ="";
+//    options += " -D TILE_LEN=" + std::to_string(m_tileLen);
+//    options += " -D " + m_localStore;
+    options += " -D WORKGROUP_SIZE=" + std::to_string(gWorkgroupSize);
+    options += " -D COLUMN_HEIGHT=" + std::to_string(2*k+1);
+    options += " -D ROW_ZERO_OFFSET=" + std::to_string(k);
+    options += " -D INT_TYPE="  STR_INT_TYPE "";
+    options += " ";
+    
+//    if (gExtendAligned)
+//        options += " -D EXTEND_ALIGNED ";
+        
     ocl->createProgramFromSource("WFO2ColsGPU.cl", options);
     m_kernel = ocl->createKernel("wfo2cols");
     
     printf("input set\n");
 }
 
-void OCLGPUWavefrontOriginal2Cols::progress(PerformanceLap& lap, long r, int& lastpercent, long cellsAllocated, long cellsAlive)
+void OCLGPUWavefrontOriginal2Cols::progress(PerformanceLap& lap, INT_TYPE r, int& lastpercent, long cellsAllocated, long cellsAlive)
 {
+    static double lastPrintLap = 0;
+    
+    double printPeriod = (gPrintPeriod > 0)? gPrintPeriod : 0.5;
+    
 #define DECIMALS_PERCENT    1000
     if (!verbose)
         return;
     
     double elapsed = lap.stop();
-    double estimated = (elapsed / r) * (m_k);
-    int percent = (r*100.0*DECIMALS_PERCENT/m_k);
+    // linear model
+//    double estimated = (elapsed / r) * (m_k);
+//    int percent = (r*100.0*DECIMALS_PERCENT/m_k);
+    // square model
+    double rd = r;
+    double kd = m_k;
+    double estimated = (elapsed / (rd*rd)) * (kd*kd);
+    int percent = (rd*rd*100.0*DECIMALS_PERCENT/(kd*kd));
     
-    
-    //if (percent != lastpercent)
+    if (elapsed > (lastPrintLap + printPeriod))
     {
+        printf((gPrintPeriod > 0)?"\n":"\r");
         //printf("\rcol %ld/%ld %.2f%% cells allocated: %ld alive: %ld elapsed: %d s  estimated: %d s    ", x, m_n, ((double)percent/DECIMALS_PERCENT), cellsAllocated, cellsAlive, (int) elapsed, (int) estimated );
-        printf("\rr %ld/%ld %.2f%% elapsed: %d s  estimated: %d s  ", r, m_k, ((double)percent/DECIMALS_PERCENT) , (int) elapsed, (int) estimated );
+        printf("r %d/%d %.2f%% elapsed: %d s  estimated: %d s  ", r, m_k, ((double)percent/DECIMALS_PERCENT),  (int) elapsed, (int) estimated );
     
         fflush(stdout);
         lastpercent = percent;
+        lastPrintLap = elapsed;
     }
 }
 
-long OCLGPUWavefrontOriginal2Cols::getDistance()
+INT_TYPE OCLGPUWavefrontOriginal2Cols::getDistance()
 {
     PerformanceLap lap;
     int lastpercent = -1;
@@ -156,17 +185,17 @@ long OCLGPUWavefrontOriginal2Cols::getDistance()
     m_queue->writeBuffer(m_buf_P, (void*) m_P, m_m);
     m_queue->writeBuffer(m_buf_T, (void*) m_T, m_n);
     
-    long h = 2*m_k+1;
+    INT_TYPE h = 2*m_k+1;
     
-    long final_d = CARTESIAN_TO_POLAR_D_D(m_m, m_n);
-    long m_top = max2(m_m,m_n);
+    INT_TYPE final_d = CARTESIAN_TO_POLAR_D_D(m_m, m_n);
+    INT_TYPE m_top = max2(m_m,m_n);
 
     setCommonArgs();
 
     if (gMeasureIterationTime)
         printf("r,time\n");
         
-    for (long r=0; r < m_k; r++)
+    for (INT_TYPE r=0; r < m_k; r++)
     {
         invokeKernel(r);
 
@@ -201,17 +230,17 @@ void OCLGPUWavefrontOriginal2Cols::setCommonArgs()
     ret = clSetKernelArg(m_kernel, 1, sizeof(cl_mem), (void *)&m_buf_T);
     CHECK_CL_ERRORS(ret);
     
-    ret = clSetKernelArg(m_kernel, 2, sizeof(cl_long), (void *)&m_m);
+    ret = clSetKernelArg(m_kernel, 2, sizeof(CL_INT_TYPE), (void *)&m_m);
     CHECK_CL_ERRORS(ret);
 
-    ret = clSetKernelArg(m_kernel, 3, sizeof(cl_long), (void *)&m_n);
+    ret = clSetKernelArg(m_kernel, 3, sizeof(CL_INT_TYPE), (void *)&m_n);
     CHECK_CL_ERRORS(ret);
     
     //long size = 2*(2*m_k+1);
 
-    long k = max2(m_m,m_n);
+    INT_TYPE k = max2(m_m,m_n);
 
-    ret = clSetKernelArg(m_kernel, 5, sizeof(cl_long), (void *)&k);
+    ret = clSetKernelArg(m_kernel, 5, sizeof(CL_INT_TYPE), (void *)&k);
     CHECK_CL_ERRORS(ret);
     
     ret = clSetKernelArg(m_kernel, 6, sizeof(cl_mem), (void *)&m_buf_W);
@@ -221,22 +250,22 @@ void OCLGPUWavefrontOriginal2Cols::setCommonArgs()
     CHECK_CL_ERRORS(ret);
 }
 
-void OCLGPUWavefrontOriginal2Cols::invokeKernel(long r)
+void OCLGPUWavefrontOriginal2Cols::invokeKernel(INT_TYPE r)
 {
     cl_int ret;
     
-    ret = clSetKernelArg(m_kernel, 4, sizeof(cl_long), (void *)&r);
+    ret = clSetKernelArg(m_kernel, 4, sizeof(CL_INT_TYPE), (void *)&r);
     CHECK_CL_ERRORS(ret);
 
-    long k = max2(m_m,m_n);
+    INT_TYPE k = max2(m_m,m_n);
     
     m_queue->invokeKernel1D(m_kernel, 2*k+1, gWorkgroupSize);
     
-    m_queue->readBuffer(m_buf_final_d_r, &m_final_d_r, sizeof(long));
+    m_queue->readBuffer(m_buf_final_d_r, &m_final_d_r, sizeof(CL_INT_TYPE));
 
 }
 
-char* OCLGPUWavefrontOriginal2Cols::getAlignmentPath(long* distance)
+char* OCLGPUWavefrontOriginal2Cols::getAlignmentPath(INT_TYPE* distance)
 {
     printf("Not implemented yet\n");
     exit(-1);
