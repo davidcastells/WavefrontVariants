55f58404f1bb42965723af99bb937e44200c2be5
David Castells-Rufas <david.castells@uab.cat>
2022-05-28 08:21:52 +0200

    int type

--- wavefront.cpp
+++ wavefront.cpp
@@ -18,7 +18,7 @@
 #include "fasta.h"
 #include "OCLUtils.h"
 #include "OCLGPUWavefrontOriginal2Cols.h"
-#include "OCLFPGAWavefrontOriginal2Cols.h"
+//#include "OCLFPGAWavefrontOriginal2Cols.h"
 
 int wavefront_classic(const char* P, const char* T, int m, int n, int k);
 int wavefront_dcr(const char* P, const char* T, int m, int n, int k);
@@ -336,13 +336,13 @@ int main(int argc, char* args[])
         OCLUtils* ocl = OCLUtils::getInstance();
         ocl->selectPlatform(gPid);
         std::string pn = ocl->getSelectedPlatformName();
-        if (OCLUtils::contains(pn, "FPGA"))
+/*        if (OCLUtils::contains(pn, "FPGA"))
         {
             OCLFPGAWavefrontOriginal2Cols aligner;
             aligner.execute(gP, gT, gK, doAlignmentPath);
         }
         else
-        {
+ */       {
             OCLGPUWavefrontOriginal2Cols aligner;
             aligner.execute(gP, gT, gK, doAlignmentPath);
         }
