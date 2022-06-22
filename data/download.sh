0a54a48c782f21e268a5b7005386642269d6ad00
David Castells-Rufas <david.castells@uab.cat>
2022-06-22 16:31:39 +0200

    cudalign data files

--- data/download.sh
+++ data/download.sh
@@ -1,10 +1,52 @@
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AF133821.1&rettype=fasta" -O AF133821.1.fasta
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AY352275.1&rettype=fasta" -O AY352275.1.fasta
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000898.1&rettype=fasta" -O NC_000898.1.fasta
-wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_007605.1&rettype=fasta" -O NC_007605.1.fasta
+#
+# Sequences from CUDAlign 1.0
+#
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AE002160.2&rettype=fasta" -O AE002160.2.fasta
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AE016879.1&rettype=fasta" -O AE016879.1.fasta
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AE017225.1&rettype=fasta" -O AE017225.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=BA000035.2&rettype=fasta" -O BA000035.2.fasta
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=BA000046.3&rettype=fasta" -O BA000046.3.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=BX927147.1&rettype=fasta" -O BX927147.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=CP000051.1&rettype=fasta" -O CP000051.1.fasta
 wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000021.7&rettype=fasta" -O NC_000021.7.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000898.1&rettype=fasta" -O NC_000898.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_007605.1&rettype=fasta" -O NC_007605.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_003064.2&rettype=fasta" -O NC_003064.2.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_003997.3&rettype=fasta" -O NC_003997.3.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_005027.1&rettype=fasta" -O NC_005027.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NT_033779.4&rettype=fasta" -O NT_033779.4.fasta 
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NT_037436.3&rettype=fasta" -O NT_037436.3.fasta
 
-wget "https://ftp.ncbi.nlm.nih.gov/genbank/gbuna1.seq.gz" -O gbuna1.seq.gz 
+#
+# Sequences from CUDAlign 3.0
+#
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000001.10&rettype=fasta" -O NC_000001.10.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_006468.3&rettype=fasta" -O NC_006468.3.fasta
+
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000019.9&rettype=fasta" -O NC_000019.9.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000020.10&rettype=fasta" -O NC_000020.10.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000021.8&rettype=fasta" -O NC_000021.8.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000022.10&rettype=fasta" -O NC_000022.10.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_006486.3&rettype=fasta" -O NC_006486.3.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_006487.3&rettype=fasta" -O NC_006487.3.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_006488.2&rettype=fasta" -O NC_006488.2.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_006489.3&rettype=fasta" -O NC_006489.3.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_017186.1&rettype=fasta" -O NC_017186.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_014318.1&rettype=fasta" -O NC_014318.1.fasta
+
+
+
+# Rucci (OpenCL FPGA)
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AF133821.1&rettype=fasta" -O AF133821.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=AY352275.1&rettype=fasta" -O AY352275.1.fasta
+wget "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000914.1&rettype=fasta" -O NC_000914.1.fasta
+
+#
+# Sequences from sadiq2020distributed
+#
+wget "http://biomirror.aarnet.edu.au/biomirror/genbank/gbgss201.seq.gz" -O gbgss201.seq.gz; gzip -d gbgss201.seq.gz
+wget "http://biomirror.aarnet.edu.au/biomirror/genbank/gbpln104.seq.gz" -O gbpln104.seq.gz; gzip -d gbpln104.seq.gz
+wget "http://biomirror.aarnet.edu.au/biomirror/genbank/gbhtg11.seq.gz" -O gbhtg11.seq.gz; gzip -d gbhtg11.seq.gz
+wget "http://biomirror.aarnet.edu.au/biomirror/genbank/gbgss116.seq.gz" -O gbgss116.seq.gz; gzip -d gbgss116.seq.gz
+wget "http://biomirror.aarnet.edu.au/biomirror/genbank/gbuna1.seq.gz" -O gbuna1.seq.gz; gzip -d gbuna1.seq.gz
+wget "http://biomirror.aarnet.edu.au/biomirror/genbank/gbinv32.seq.gz" -O gbinv32.seq.gz; gzip -d gbinv32.seq.gz
